ac81329cb9aea0305cd8e4548e8f0f907afd39fb
HeSraOne <141596127+HeSraOne@users.noreply.github.com>
2023-09-19 08:59:26 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -86,12 +86,40 @@ const timer = () => {
   }, 1000);
 };
 
+const scrollToBottomSlowly = (currentScroll, targetScroll, step = 5) => {
+  if (currentScroll < targetScroll) {
+    const newScroll = currentScroll + step;
+    const remainingScroll = targetScroll - newScroll;
+
+    // Scroll to the bottom if the remaining scroll is less than the step
+    if (remainingScroll < step) {
+      window.scrollTo({
+        top: targetScroll,
+        behavior: "smooth",
+      });
+      return;
+    }
+
+    window.scrollTo({
+      top: newScroll,
+      behavior: "smooth",
+    });
+
+    setTimeout(() => {
+      scrollToBottomSlowly(newScroll, targetScroll);
+    }, 10);
+  }
+};
+
 const buka = async () => {
   document.getElementById("tombol-musik").style.display = "block";
   audio.play();
   AOS.init();
   await login();
   timer();
+
+  const targetScroll = document.body.scrollHeight;
+  scrollToBottomSlowly(0, targetScroll);
 };
 
 const play = (btn) => {
