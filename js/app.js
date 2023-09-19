16556b8415c661e084a7fc296a8033ea422a0bba
HeSraOne <141596127+HeSraOne@users.noreply.github.com>
2023-09-19 10:16:09 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -88,7 +88,7 @@ const timer = () => {
 
 function scrollToBottomSmoothly() {
   const scrollStep = 5;
-  const scrollInterval = 20; // milliseconds
+  const scrollInterval = 5; // milliseconds
 
   const scrollHeight = document.body.scrollHeight;
   let scrolled = 0;
